bec9e9b26eaf14769220ceea460316b6fe6c71ec
Johncakes <johnnygamesover9000@gmail.com>
2025-11-10 15:46:29 +0900

    hotfix: delete ConferenceIntroPage nav
    - Prettier for better readability
    - fixed issue with navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,48 @@
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import IntroductionPage from './pages/IntroductionPage';
-import LotteryPage from './pages/LotteryPage';
-import ConferenceIntroPage from './pages/ConferenceIntroPage';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import IntroductionPage from "./pages/IntroductionPage";
+import LotteryPage from "./pages/LotteryPage";
 
 const navigationItems = [
-  { id: 'nav-home', label: 'Home', path: '/' },
-  { id: 'nav-intro', label: '소개', title: '소개 및 일정', subTitle: '2025 그리디 컨퍼런스', path: '/ConferenceIntroPage' },
-  { id: 'nav-lottery', label: '추첨', title: '경품 추첨', subTitle: '그린이 키 맞추기', path: '/LotteryPage' },
-  { id: 'nav-about', label: 'About us', title: 'ABOUT US', subTitle: '그리디 알아보기', path: 'https://greedy-team.netlify.app/' },
+  { id: "nav-home", label: "Home", path: "/" },
+  {
+    id: "nav-intro",
+    label: "소개",
+    title: "소개 및 일정",
+    subTitle: "2025 그리디 컨퍼런스",
+    path: "/IntroductionPage",
+  },
+  {
+    id: "nav-lottery",
+    label: "추첨",
+    title: "경품 추첨",
+    subTitle: "그린이 키 맞추기",
+    path: "/LotteryPage",
+  },
+  {
+    id: "nav-about",
+    label: "About us",
+    title: "ABOUT US",
+    subTitle: "그리디 알아보기",
+    path: "https://greedy-team.netlify.app/",
+  },
 ];
 
 function App() {
   return (
     <div className="min-h-screen w-full overflow-x-hidden">
-      <Header navigationItems={navigationItems}/>
+      <Header navigationItems={navigationItems} />
       <Routes>
-        <Route path="/" element={<LandingPage navigationItems={navigationItems} />} />
+        <Route
+          path="/"
+          element={<LandingPage navigationItems={navigationItems} />}
+        />
         <Route path="/IntroductionPage" element={<IntroductionPage />} />
         <Route path="/LotteryPage" element={<LotteryPage />} />
-        <Route path="/ConferenceIntroPage" element={<ConferenceIntroPage />} />
       </Routes>
-      <Footer navigationItems={navigationItems}/>
+      <Footer navigationItems={navigationItems} />
     </div>
   );
 }
