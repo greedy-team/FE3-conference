8c578a0396818a9fdf2f3b2fffe1e5529020bbaa
dkr-sjr <rkdrjs4637@naver.com>
2025-11-11 21:38:24 +0900

    refactor: 메인 헤더 사이즈 제한

--- src/components/Header/MainHeader.jsx
+++ src/components/Header/MainHeader.jsx
@@ -13,12 +13,12 @@ export default function MainHeader() {
           </div>
         </div>
 
-        <div className="flex-1 flex flex-col px-[10px] lg:hidden">
+        <div className="flex-1 flex flex-col px-[10px] lg:hidden max-[344px]:hidden">
           <div className="flex-1 text-center border-b-1 border-[#333333]" />
           <div className="flex-1 text-center" />
         </div>
 
-        <div className="text-center text-[#333333] font-pretendard tracking-[-0.07em] text-[18pt] font-bold lg:hidden">
+        <div className="text-center text-[#333333] font-pretendard tracking-[-0.07em] text-[18pt] font-bold lg:hidden max-[344px]:hidden">
           11.19-11.20
         </div>
       </div>
