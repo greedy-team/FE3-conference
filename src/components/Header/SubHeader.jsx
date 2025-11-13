6c402ead6c7a9db28ed14c9f2d3a3fddcd042b3d
dkr-sjr <rkdrjs4637@naver.com>
2025-11-13 22:48:22 +0900

    refactor(Header) : 2xl 기준 추가

--- src/components/Header/SubHeader.jsx
+++ src/components/Header/SubHeader.jsx
@@ -5,7 +5,7 @@ import { useLocation, useNavigate, Link } from "react-router-dom";
 
 function TextBox({ children }) {
   return (
-    <div className="flex text-center text-black font-pretendard tracking-[-0.07em] text-xs font-semibold lg:text-xl">
+    <div className="flex text-center text-black font-pretendard tracking-[-0.07em] text-xs font-semibold lg:text-xl 2xl:text-4xl">
       {children}
     </div>
   );
@@ -19,9 +19,9 @@ export default function SubHeader({ navigationItems }) {
     <div className="sticky left-0 right-0 top-0 z-50 border-b border-neutral-300">
       <div className="flex flex-row items-center justify-between p-4 bg-white">
         <Link to={"/"} className="flex items-center ">
-          <img className="w-10 h-10" src={greedyIcon} />
-          <div className="w-3"></div>
-          <div className="text-center text-[#333333] font-pretendard tracking-[-0.02em] text-lg font-bold">
+          <img className="w-10 h-10 2xl:w-25 2xl:h-25" src={greedyIcon} />
+          <div className="w-3 2xl:w-6"></div>
+          <div className="text-center text-[#333333] font-pretendard tracking-[-0.02em] text-lg font-bold 2xl:text-5xl">
             세종 그리디콘
           </div>
         </Link>
